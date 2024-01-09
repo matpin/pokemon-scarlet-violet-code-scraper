7c3abd4c0b9814001ed86df5e268cf2797c24e03
matpin <stamkouk92@gmail.com>
2024-01-09 10:00:43 +0200

    Appending the td's on an array variable

--- scraper.py
+++ scraper.py
@@ -9,6 +9,10 @@ def scrape_page(soup, code_details):
     for code_row in code_rows:
         code_tds = code_row.find_all("td")
 
+        details = []
+        for code_td in code_tds:
+            details.append(code_td.text)
+
 
 # the url of the home page of the target website
 base_url = 'https://www.nintendolife.com/guides/pokemon-scarlet-and-violet-mystery-gift-codes-list'
